d5b6b15a482e2629e781f4dc4195c22dc5fa233c
danilk09 <134243371+danilk09@users.noreply.github.com>
2026-03-27 07:45:53 -1000

    list contacts page mockup finished in 19 minutes

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Contact } from '@/lib/validationSchemas';
+import { Card, Image } from "react-bootstrap";
+
+/* Renders a single row in the List Stuff table. See list/page.tsx. */
+const ContactCard = ({ firstName, lastName, address, image, description }: Contact) => (
+    <Card className="h-100">
+        <Card.Header>
+            <Image src={image} width={75} alt="Contact Image" />
+        </Card.Header>
+        <Card.Body>
+            <Card.Title>{firstName} {lastName}</Card.Title>
+            <Card.Subtitle>{address}</Card.Subtitle>
+            <Card.Text>{description}</Card.Text>
+        </Card.Body>
+    </Card>
+);
+
+export default ContactCard;
